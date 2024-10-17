0afa330774f22cbb1a0fabf84aa9e2b1f9b325e9
Matheus <matheus@i2.ninja>
2024-10-17 20:45:04 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MelhorPreco',
+  webDir: 'dist'
+};
+
+export default config;
